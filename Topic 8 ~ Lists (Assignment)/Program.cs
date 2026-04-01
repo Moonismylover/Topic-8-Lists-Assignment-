7b132138678159a6628acf648bf27fed6fa9bbb9
Moonismylover <Angelpreetthind9@gmail.com>
2026-04-01 17:46:45 -0400

    1234567890

--- Topic 8 ~ Lists (Assignment)/Program.cs
+++ Topic 8 ~ Lists (Assignment)/Program.cs
@@ -195,32 +195,3 @@
         }
     }
 }
-
-
-
-/**
-Lists Programming Assignments
-
-Create a single Console (.NET Framework) program and include the following assignments in it. Make
-sure to label each part using comments and an appropriate WriteLine() statement.
-Programming Assignment 1 – Reading in Colors
-Create a program that uses a For Loop to read in 5 colors and store it in a List of strings. Print all
-elements in the List on the same line, separated by a comma. Generate a random number and use that
-number to print out a random color from the List.
-Programming Assignment 2 – Random Numbers
-Create a List of integers. Asks the user to enter how many numbers they need. Next, ask the user for a
-maximum and minimum value. Use a for loop to fill the List with random values within that range
-(inclusive). Once the List is filled with values, do the following using a separate loop for each (make sure
-to include an explanatory message for each part):
-1. Print the List to the screen on one line.
-2. Prompt the user for a number and count the number of times that number appears in the List.
-a. Display the number of times that number appeared in a formatted
-Console.WriteLine() statement.
-
-3. Prompt the user for a number and replace all occurrences of that number with zero.
-4. Print the List to the screen on one line.
-5. Using a loop, reset each value in the List with zeros.
-6. Print the List to the screen on one line.
-7. Use a loop to fill the List again with new random numbers.
-8. Print the List to the screen on one line.
-**/
